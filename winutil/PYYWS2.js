f4179223c4c890e444ecaa5c2d19206b2ef0fa63
electronixtar <electronixtar@fd9c5bdc-9f31-11dd-bc7a-dd6b2ed99ab5>
2009-05-14 12:17:55 +0000

    ping.fm auth segregation

--- winutil/PYYWS2.js
+++ winutil/PYYWS2.js
@@ -4,9 +4,15 @@
  * Copyleft @ 2009
  */
 
+//put your Ping.fm authentication key (https://ping.fm/key/) here
+var AUTH = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-XXXXXXXXXX";
+
+
+/* --------------------- WinOMeter part --------------------- */
+
 var shell   = new ActiveXObject("WScript.Shell");
 var history = shell.RegRead("HKEY_CURRENT_USER\\Software\\Tomas Jelinek\\WinOMeter\\History").toArray();
-var dupcheck_key = "HKEY_CURRENT_USER\\Software\\Tomas Jelinek\\WinOMeter\\last_PYYWS2"
+var dupcheck_key = "HKEY_CURRENT_USER\\Software\\Tomas Jelinek\\WinOMeter\\last_PYYWS2";
 
 var item, d = new Date();
 var s = [d.getDate()-1, d.getMonth()+1, d.getFullYear() % 256, Math.floor(d.getFullYear()/256)].toString();
@@ -62,16 +68,17 @@ decode_ieee754 = function (a)
     return (s?-1:1) * m * Math.pow(2, e-mLen);
 };
 
-kp = rs(item.slice(4, 8));
-mt = decode_ieee754(item.slice(8, 12))/1000;
-rc = rs(item.slice(12, 14));
-mc = rs(item.slice(14, 16));
-lc = rs(item.slice(16, 20));
-ut = rs(item.slice(20, 24));
+
+kp = rs(item.slice(4, 8));                      //key presses
+mt = decode_ieee754(item.slice(8, 12))/1000;    //Mouse Trajectory
+rc = rs(item.slice(12, 14));	                //right clicks	
+mc = rs(item.slice(14, 16));	                //middle clicks	
+lc = rs(item.slice(16, 20));	                //left clicks
+ut = rs(item.slice(20, 24));	                //uptime in seconds
 uth = Math.floor(ut / 3600)
 utm = Math.floor( (ut-uth*3600)/60 )
 uts = ut - uth*3600 - utm*60
-ut = uth+'h'+utm+'m'+uts+'s'
+ut = uth+'h'+utm+'m'+uts+'s'                    //uptime in 12h34m56s
 
 
 /* --------------------- ping.fm part --------------------- */
@@ -82,7 +89,7 @@ function post_to_pingfm(text)
 {
     xhr.Open("POST", "http://api.ping.fm/v1/user.post", 0);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    xhr.Send("api_key=0b48e29f10e6f46acb1d9974f32be075&user_app_key=9f21a5b1fa386c25ab4296652b9aaed3-1214056623&post_method=default&body=" + encodeURIComponent(text))
+    xhr.Send("api_key=0b48e29f10e6f46acb1d9974f32be075&user_app_key=" + AUTH + "&post_method=default&body=" + encodeURIComponent(text))
     return xhr.getResponseBody
 }
 
@@ -100,7 +107,7 @@ try
     if(shell.RegRead(dupcheck_key)!=s)
     {
         //WScript.Echo
-        post_to_pingfm("WinOMeter yesterday for @est: key presses: "+kp+", mouse trajectory: "+mt+"m, L/M/R clicks: "+lc+"/"+mc+"/"+rc+", uptime: "+ut+" #PYYWS");
+        WScript.Echo("WinOMeter yesterday for @est: key presses: "+kp+", mouse trajectory: "+mt+"m, L/M/R clicks: "+lc+"/"+mc+"/"+rc+", uptime: "+ut+" #PYYWS");
     }
     shell.RegWrite(dupcheck_key, s);
 }
